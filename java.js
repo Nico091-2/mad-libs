0a2c55ea54cb955ceb4868665f424405f89551cb
Nico091-2 <327nvelasco@frhsd.com>
2025-12-16 14:03:52 -0500

    ww

--- java.js
+++ java.js
@@ -1,9 +1,14 @@
 var button = document.getElementById('story');
 button.addEventListener("click", getwords);
-function getwords(){
-    console.log("word");
+function getwords() {
+    var words = document.querySelectorAll("input[type=text]")
+    var placeholders = document.querySelectorAll(".userInput")
+    console.log(words);
+
+    placeholders.forEach((placeholder, index) => {
+        console.log(placeholder, index);
+    });
+
 }
 
-var words = document.querySelectorAll("input[type=text]")
-console.log (words);
 
