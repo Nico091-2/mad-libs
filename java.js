1621cb7da487eb9518f13e9bf6a49b0111928334
Nico091-2 <327nvelasco@frhsd.com>
2025-12-18 12:20:13 -0500

    changes

--- java.js
+++ java.js
@@ -3,11 +3,19 @@ button.addEventListener("click", getwords);
 function getwords() {
     var words = document.querySelectorAll("input[type=text]")
     var placeholders = document.querySelectorAll(".userInput")
-    console.log(words);
+    // console.log(words);
 
     placeholders.forEach((placeholder, index) => {
-        console.log(placeholder, index);
+        // console.log(placeholder, index);
     });
+
+    display:none;
+
+    placeholders.forEach((placeholder,index))=> {
+
+        placeholder.innerHTML = words[index].ariaValueMax;
+    }
+    //show hidden story
 
 }
 
